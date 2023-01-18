eb7cf770db983c6b55353451badef30b00711a9d
aparra19 <angelparra67@gmail.com>
2023-01-17 20:12:51 -0400

    Insertar carousel a la vista principal

---
+++ src/components/CarouselHome.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container-fluid">
+    <div id="carouselExampleCaptions" class="carousel slide mt-5 mb-5" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+          aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+          aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="../../public/slider/slider-1.jpg" class="d-block w-100 img-slider" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Información y Detalles</h5>
+            <p>Enterate de las peliculas que estan dando de que hablar.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="../../public/slider/slider-2.jpg" class="d-block w-100 img-slider" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Trailers</h5>
+            <p>Disfruta de los mejores trailers de tus peliculas favoritas</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="../../public/slider/slider-3.jpg" class="d-block w-100 img-slider" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Posters</h5>
+            <p>Las mejores imagenes de las peliculas que a ti te gustan</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .img-slider {
+    height: 40rem;
+    opacity: 0.9;
+    overflow: hidden;
+  }
+  h5 {
+    color: white;
+    font-weight: bold;
+  }
+  p {
+    color: white;
+    font-weight: bold;
+  }
+</style>
