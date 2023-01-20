4d3704d14895f3b1bff51a8b801d3dcec80067af
aparra19 <angelparra67@gmail.com>
2023-01-20 16:23:09 -0400

    Anexo de layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <MenuTv />
+    <slot name="home"></slot>
+    <slot name="movieview"></slot>
+    <slot name="pelis-all"></slot>
+    <slot name="pelis-genre"></slot>
+    <FooterTv />
+</template>
+<script>
+import MenuTv from '@/components/MenuTv.vue'
+import FooterTv from '@/components/FooterTv.vue'
+export default {
+  components: { MenuTv, FooterTv }
+}
+</script>
