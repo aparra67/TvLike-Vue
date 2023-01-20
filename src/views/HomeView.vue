7b042057121bbd5c4d02d118e8ac8788378f2652
aparra19 <angelparra67@gmail.com>
2023-01-20 16:27:58 -0400

    Optimizar codigo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,36 @@
 <template>
   <!-- La vista de 'home' se cargan los componentes que necesita la vista 'MenuTv' y 'ContentHome'-->
-  <div class="home">
-    <MenuTv/>
-    <CarouselHome />
-    <ContentHome/>
-    <FooterTv/>
-  </div>
+  <MainLayout>
+    <template #home>
+      <div class=" container-fluid back p-3 m-2">
+        <h1>tvlike</h1>
+        <CarouselHome />
+      </div>
+      <ContentHome/>
+    </template>
+  </MainLayout>
 </template>
 
 <script>
-import MenuTv from '@/components/MenuTv.vue'
+import MainLayout from '@/layouts/MainLayout.vue'
 import ContentHome from '@/components/ContentHome.vue'
-import FooterTv from '@/components/FooterTv.vue'
 import CarouselHome from '@/components/CarouselHome.vue'
 export default {
   name: 'HomeView',
-  components: { MenuTv, ContentHome, FooterTv, CarouselHome }
+  components: { MainLayout, CarouselHome, ContentHome }
 }
 </script>
+<style>
+.back {
+  background: #1b2557d9;
+  border-radius: 10px;
+}
+h1 {
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 80px;
+  letter-spacing: 5px;
+  color: rgb(209, 13, 13);
+}
+</style>
