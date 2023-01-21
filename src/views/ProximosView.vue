89c0c86df18115b06a54a22820092df8357972a5
aparra19 <angelparra67@gmail.com>
2023-01-20 20:41:51 -0400

    Agregar vista de Proximos estrenos

---
+++ src/views/ProximosView.vue
@@ -0,0 +1,32 @@
+<template>
+  <MainLayout>
+    <template #next>
+      <div class="container-fluid back m-2">
+        <h1>tvlike</h1>
+        <img src="../../public/no-disponible.jpg" alt="No disponible" />
+      </div>
+    </template>
+  </MainLayout>
+</template>
+<script>
+import MainLayout from '@/layouts/MainLayout.vue'
+export default {
+  components: { MainLayout }
+}
+</script>
+<style>
+.back {
+  background: #1b2557d9;
+  border-radius: 10px;
+  padding: 7rem 0;
+}
+
+h1 {
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 80px;
+  letter-spacing: 5px;
+  color: rgb(209, 13, 13);
+}
+</style>
