0f7f245ecf4ca38b7435757799e5f555c4a67ba9
aparra19 <angelparra67@gmail.com>
2023-01-20 20:52:22 -0400

    Modificar los estilos y anexar un slot

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,13 @@
 <template>
+  <div class="content-main">
     <MenuTv />
     <slot name="home"></slot>
     <slot name="movieview"></slot>
     <slot name="pelis-all"></slot>
     <slot name="pelis-genre"></slot>
+    <slot name="next"></slot>
     <FooterTv />
+  </div>
 </template>
 <script>
 import MenuTv from '@/components/MenuTv.vue'
@@ -13,3 +16,11 @@ export default {
   components: { MenuTv, FooterTv }
 }
 </script>
+<style>
+.content-main {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+</style>
