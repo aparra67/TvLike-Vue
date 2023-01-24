23c995ecd996ba296b338d5d229d468624e88dd1
aparra19 <angelparra67@gmail.com>
2023-01-24 10:45:42 -0400

    Anexar un spinner como componente

---
+++ src/components/SpinnerComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="spinner-content">
+    <div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <h3 class="mt-2">Cargando...</h3>
+  </div>
+</template>
+<style scoped>
+.spinner-content {
+  width: 100vw;
+  height: 100vmin;
+  background: rgba(39, 70, 110, 0.733);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
